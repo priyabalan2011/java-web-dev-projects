8f96bb5b4213384d7fdc5d4755bb5bf27052834f
Balan Karuppasamy <agnibalan@gmail.com>
2023-09-11 17:27:58 -0500

    unittest commit

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -1,11 +1,42 @@
 package org.launchcode;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 class CarTest {
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
+    @Test
+    public void emptyTest() {
+        assertEquals(10,10,.001);
+    }
     //TODO: constructor sets gasTankLevel properly
+
+    @Test
+    public void testInitialGasTank() {
+        Car test_car = new Car("Toyota", "Prius", 10, 50);
+        assertEquals(10, test_car.getGasTankLevel(), .001);
+    }
+
+
+    Car test_car;
+
+    @BeforeEach
+    public void createCarObject() {
+        test_car = new Car("Toyota", "Prius", 10, 50);
+    }
+
     //TODO: gasTankLevel is accurate after driving within tank range
+   @Test
+    public void testInitialGasTankFalse()
+    {
+        assertFalse(test_car.getGasTankLevel()==0);
+    }
+    @Test
+    public void testInitialGasTankTrue()
+    {
+        assertTrue(test_car.getGasTankLevel()==10);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
 }
--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,63 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(" "));
+    }
+    @Test
+    public void StringWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[lauchcode]"));
+    }
+    @Test
+    public void onlyOpenBracketsReturnsFalse()
+    {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch[code"));
+    }
+    @Test
+    public void onlyCloseBracketsReturnsFalse()
+    {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void mismatchBracketsReturnsFalse()
+    {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void unequalBracketsReturnsFalse()
+    {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch[]"));
+    }
+    @Test
+    public void twoBracketsReturnsTrue()
+    {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+    @Test
+    public void withinBracketsReturnsTrue()
+    {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+    @Test
+    public void bracketsOfBracketsWithinBracketsReturnsTrue()
+    {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]][]"));
+    }
+    @Test
+    public void tripleBracketsWithinBracketsReturnsTrue()
+    {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+    @Test
+    public void twoTripleBracketsWithinBracketsReturnsTrue()
+    {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[][]]]"));
+    }
+
 }
