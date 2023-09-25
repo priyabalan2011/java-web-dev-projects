c3145b0870c53df9064bb471a0645ed083009068
Balan Karuppasamy <agnibalan@gmail.com>
2023-09-25 14:56:53 -0500

    exception added

---
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/ArithmeticException.java
@@ -0,0 +1,8 @@
+package org.launchcode;
+
+public class ArithmeticException extends Exception{
+    ArithmeticException(String message)
+    {
+        super(message);
+    }
+}
--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,10 +1,14 @@
 package org.launchcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+       System.out.println(Divide(25,5));
+
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -13,15 +17,56 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+        //System.out.println(CheckFileExtension(""));
+        for(Map.Entry<String,String> student:studentFiles.entrySet())
+        {
+            System.out.println(CheckFileExtension(student.getValue()));
+        }
     }
 
-    public static void Divide(int x, int y)
+    public static int Divide(int x, int y)
     {
         // Write code here!
+
+        if(y<=0) {
+            try {
+
+                throw new ArithmeticException("Y value is below zero");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return (x / y);
     }
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        boolean str;
+        if(fileName.equals("") || fileName.equals("null"))
+        {
+            try{
+                throw  new Exception("Filename is empty");
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                return -1;
+            }
+
+        }
+        str=fileName.contains(".java");
+
+        if(str)
+        {
+                return 1;
+        }
+        else {
+            return 0;
+        }
+
+
     }
 }
